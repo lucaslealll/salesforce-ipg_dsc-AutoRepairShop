0266070c9a8f5cd2ec459d94ba53684bd1943231
lucasoal <lucasoalzst@gmail.com>
2023-06-08 20:08:33 +0100

    unused functions removed

--- force-app/main/default/lwc/lwc_sample/lwc_sample.js
+++ force-app/main/default/lwc/lwc_sample/lwc_sample.js
@@ -1,12 +1,3 @@
 import { LightningElement } from 'lwc';
 
-function openDialog() {
-    document.getElementById('dialog').style.display = 'block';
-}
-
-function closeDialog() {
-    document.getElementById('dialog').style.display = 'none';
-}
-
-
 export default class Lwc_sample extends LightningElement { }
