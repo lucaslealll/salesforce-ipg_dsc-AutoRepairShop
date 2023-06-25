2a6c40027c9690078ed14bfb63a8a9bb4f84027c
lucasoal <lucasoalzst@gmail.com>
2023-06-25 17:09:29 +0100

    LWC to add replacement parts on the system implemented. Everything works as expected

--- force-app/main/default/lwc/addReplacementPartsV2/addReplacementPartsV2.js
+++ force-app/main/default/lwc/addReplacementPartsV2/addReplacementPartsV2.js
@@ -1,14 +1,14 @@
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 import { LightningElement, api, track } from 'lwc';
-//import the apex controller for this lwc
 import saveReplacementParts from '@salesforce/apex/ReplacementPartController_Main.saveNewReplacementParts';
 
-export default class AddReplacementParts extends LightningElement {
+export default class AddReplacementPartsV2 extends LightningElement {
     @api recordId
+
     //add a new form line
-    addRow() {//console.log('add.addRow',JSON.parse(JSON.stringify(this.replacePartsFormInfo)));
+    addRow() {
         //add a new row with empty fields
-        this.replacePartsFormInfo.push({
+        this.replacementPartsV2FormInfo.push({
             Name: '',
             UnitCost__c: '',
             Part_Number__c: '',
@@ -21,17 +21,16 @@ export default class AddReplacementParts extends LightningElement {
             Minimum_Stock_Level__c: '',
             Notes__c: '',
         });
-        //console.log('add.addRow',JSON.parse(JSON.stringify(this.replacePartsFormInfo)));
     }
     //delete a new form line
     deleteRow(event) {
         //if have more than one row delete the current row
         var rowIndex = event.currentTarget.dataset.index;
-        if (this.replacePartsFormInfo.length > 1) {
-            this.replacePartsFormInfo.splice(rowIndex, 1);
+        if (this.replacementPartsV2FormInfo.length > 1) {
+            this.replacementPartsV2FormInfo.splice(rowIndex, 1);
             //else if have just one row delete the current row data
-        } else if (this.replacePartsFormInfo.length == 1) {
-            this.replacePartsFormInfo = [{
+        } else if (this.replacementPartsV2FormInfo.length == 1) {
+            this.replacementPartsV2FormInfo = [{
                 Name: '',
                 UnitCost__c: '',
                 Part_Number__c: '',
@@ -47,7 +46,7 @@ export default class AddReplacementParts extends LightningElement {
         }
     }
     //track  variables value from html
-    @track replacePartsFormInfo = [{
+    @track replacementPartsV2FormInfo = [{
         Name: '',
         UnitCost__c: '',
         Part_Number__c: '',
@@ -62,8 +61,8 @@ export default class AddReplacementParts extends LightningElement {
     }];
     //remove all rows
     removeAllRows() {
-        let replacePartsFormInfo = [];
-        this.replacePartsFormInfo = replacePartsFormInfo;
+        let replacementPartsV2FormInfo = [];
+        this.replacementPartsV2FormInfo = replacementPartsV2FormInfo;
         this.addRow();
     }
     //handle the change of the values of each field and row to pick up the last inserted
@@ -71,61 +70,60 @@ export default class AddReplacementParts extends LightningElement {
         var rowIndex = event.currentTarget.dataset.index;
         console.log('>>>>>>>>>>>>>: ');
         if (event.target.name === 'Name') {
-            this.techniciansFormInfo[rowIndex].Name = event.target.value;
+            this.replacementPartsV2FormInfo[rowIndex].Name = event.target.value;
         }
         else if (event.target.name === 'UnitCost__c') {
-            this.techniciansFormInfo[rowIndex].UnitCost__c = event.target.value;
+            this.replacementPartsV2FormInfo[rowIndex].UnitCost__c = event.target.value;
         }
         else if (event.target.name === 'Part_Number__c') {
-            this.techniciansFormInfo[rowIndex].Part_Number__c = event.target.value;
+            this.replacementPartsV2FormInfo[rowIndex].Part_Number__c = event.target.value;
         }
         else if (event.target.name === 'VehicleMake__c') {
-            this.techniciansFormInfo[rowIndex].VehicleMake__c = event.target.value;
+            this.replacementPartsV2FormInfo[rowIndex].VehicleMake__c = event.target.value;
         }
         else if (event.target.name === 'Vehicle_Model__c') {
-            this.techniciansFormInfo[rowIndex].Vehicle_Model__c = event.target.value;
+            this.replacementPartsV2FormInfo[rowIndex].Vehicle_Model__c = event.target.value;
         }
         else if (event.target.name === 'Vehicle_Year__c') {
-            this.techniciansFormInfo[rowIndex].Vehicle_Year__c = event.target.value;
+            this.replacementPartsV2FormInfo[rowIndex].Vehicle_Year__c = event.target.value;
         }
         else if (event.target.name === 'Stock__c') {
-            this.techniciansFormInfo[rowIndex].Stock__c = event.target.value;
+            this.replacementPartsV2FormInfo[rowIndex].Stock__c = event.target.value;
         }
         else if (event.target.name === 'Supplier__c') {
-            this.techniciansFormInfo[rowIndex].Supplier__c = event.target.value;
+            this.replacementPartsV2FormInfo[rowIndex].Supplier__c = event.target.value;
         }
         else if (event.target.name === 'Stock_Location__c') {
-            this.techniciansFormInfo[rowIndex].Stock_Location__c = event.target.value;
+            this.replacementPartsV2FormInfo[rowIndex].Stock_Location__c = event.target.value;
         }
         else if (event.target.name === 'Minimum_Stock_Level__c') {
-            this.techniciansFormInfo[rowIndex].Minimum_Stock_Level__c = event.target.value;
+            this.replacementPartsV2FormInfo[rowIndex].Minimum_Stock_Level__c = event.target.value;
         }
         else if (event.target.name === 'Notes__c') {
-            this.techniciansFormInfo[rowIndex].Notes__c = event.target.value;
+            this.replacementPartsV2FormInfo[rowIndex].Notes__c = event.target.value;
         }
-        //console.log('Aqui: ', event.target.name);console.log('Aqui: ', event.target.value);
-        //console.log('handleChange',JSON.parse(JSON.stringify(this.replacePartsFormInfo)));
     }
     //save new records
     handleSave() {
         saveReplacementParts({
-            replacePartsFormInfo: JSON.stringify(this.replacePartsFormInfo), ID: this.recordId
+            replacementPartsV2FormInfo: JSON.stringify(this.replacementPartsV2FormInfo), ID: this.recordId
         })
             .then(result => {
                 if (result) {
                     this.showNotification('Replacement Part created successfully',
-                        'Replacement Part were successfully inserted', 'success');
+                        'Replacement part successfully inserted', 'success');
                     //create a new event
                     const refreshEvent = new CustomEvent('refresh');
                     //dispatch the event
                     this.dispatchEvent(refreshEvent);
                 }
+                console.log('result: ', result);
                 //if something went wrong send error to the user
             }).catch(error => {
                 this.showNotification('Error creating record',
                     'Something went wrong ' + error.body.message, 'error');
             });
-        this.replacePartsFormInfo = [{
+        this.replacementPartsV2FormInfo = [{
             Name: '',
             UnitCost__c: '',
             Part_Number__c: '',
