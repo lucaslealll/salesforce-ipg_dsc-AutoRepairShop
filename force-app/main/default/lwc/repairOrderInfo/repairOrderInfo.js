3c9805d906d9bf183ea50d664e12c62c2fec6350
lucasoal <lucasoalzst@gmail.com>
2023-06-26 13:25:35 +0100

    update to lwc work at homepage

--- force-app/main/default/lwc/repairOrderInfo/repairOrderInfo.js
+++ force-app/main/default/lwc/repairOrderInfo/repairOrderInfo.js
@@ -4,15 +4,23 @@ import { LightningElement, api, wire } from "lwc";
 
 const COLUMNS = [
     { label: 'Name', fieldName: 'Name', cellAttributes: { alignment: 'center' } },
-    { label: 'Tech Name', fieldName: 'TechnicianFK__c', cellAttributes: { alignment: 'center' } },
-    { label: 'Repair Order Utilization', fieldName: 'UtilizationRepairOrder__c', cellAttributes: { alignment: 'center' } },
+    { label: 'Due Date', fieldName: 'Due_Date__c', cellAttributes: { alignment: 'center' } },
+    { label: 'Car Brand', fieldName: 'Brand__c', cellAttributes: { alignment: 'center' } },
+    { label: 'Car Model', fieldName: 'BrandModel__c', cellAttributes: { alignment: 'center' } },
+    { label: 'Car Plate', fieldName: 'CarPlate__c', cellAttributes: { alignment: 'center' } },
+    { label: 'Status', fieldName: 'RepairStatus__c', cellAttributes: { alignment: 'center' } },
+    { label: 'Replacement Parts', fieldName: 'SelectedReplacementParts__c', cellAttributes: { alignment: 'center' } },
+    { label: 'Aux. Technicians', fieldName: 'SelectedTechniciansIds__c', cellAttributes: { alignment: 'center' } },
+    { label: 'Vehicle Test', fieldName: 'VehicleTest__c', cellAttributes: { alignment: 'center' } },
+    { label: 'Utilization', fieldName: 'UtilizationRepairOrder__c', cellAttributes: { alignment: 'center' } },
 ];
 
 export default class RepairOrderInfo extends LightningElement {
     tabColumns = COLUMNS;
     @api recordId;
 
-    @wire(getRepairOrderMain, { repairOrderId: '$recordId' })
+    // @wire(getRepairOrderMain, { repairOrderId: '$recordId' })
+    @wire(getRepairOrderMain, {})
     repairOrder;
     @api
     refreshList() {
