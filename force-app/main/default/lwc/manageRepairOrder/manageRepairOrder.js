9759a83dbc03fd095e73d01dbb8d72cbf72dd7a0
lucasoal <lucasoalzst@gmail.com>
2023-06-28 13:19:19 +0100

    displaying data

--- force-app/main/default/lwc/manageRepairOrder/manageRepairOrder.js
+++ force-app/main/default/lwc/manageRepairOrder/manageRepairOrder.js
@@ -1,21 +1,38 @@
-import { LightningElement, api } from 'lwc';
+import { LightningElement, api, wire} from 'lwc';
+import { NavigationMixin } from 'lightning/navigation';
+import { refreshApex } from "@salesforce/apex";
+import getRepOrdDataTable from "@salesforce/apex/ManRepOrdTechController_Main.getRepOrdDataTable";
 
-export default class ManageRepairOrder extends LightningElement {
+const actions = [
+    { label: 'View', name: 'view' },
+    { label: 'Edit', name: 'edit' },
+    { label: 'Delete', name: 'delete' },
+];
+
+const COLUMNS = [
+    { label: 'Rep. Order', fieldName: 'Name', cellAttributes: { alignment: 'left' } },
+    { label: 'Technician', fieldName: 'TechnicianFK__c', cellAttributes: { alignment: 'left' } },
+    { label: 'Parts', fieldName: 'ReplacementPart_FK__c', cellAttributes: { alignment: 'left' } },
+    { label: 'Actions', type: 'action', typeAttributes: { rowActions: actions }, cellAttributes: { alignment: 'center' } },
+];
+
+export default class ManRepOrdTech extends LightningElement {
+    error;
+    searchString;
+    initialRecords;
+    
+    tabColumns = COLUMNS;
     @api recordId;
 
-    value = false;
-    techRefresh = false;
 
-    handleRefresh(event) {
-        this.removeAllRows('c-replacement-part-info');
-        this.value = true;
-    }
-    handleRefreshTechnician(event) {
-        this.removeAllRows('c-technicians-info');
-        this.techRefresh = true;
-    }
-    removeAllRows(component) {
-        console.log('component', component);
-        this.template.querySelector(component).refreshList();
+    // @wire(getRepOrdDataTable, { mrocOutput: '$recordId' })
+    @wire(getRepOrdDataTable, { mrocOutput: 'a026700001Iuw93AAB' })
+    mrocOutput;
+
+    @api
+    refreshList() {
+        console.log('Chamou o refresh repair order');
+        refreshApex(this.controllerOutput);
+        console.log('repair order result:', this.controllerOutput);
     }
 }
