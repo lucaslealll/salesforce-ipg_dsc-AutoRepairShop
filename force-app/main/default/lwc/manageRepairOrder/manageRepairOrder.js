51efa72cfd7c88db539c679c1495414c8ee1d692
lucasoal <lucasoalzst@gmail.com>
2023-06-29 11:15:04 +0100

    actions an search filter are working!!!

--- force-app/main/default/lwc/manageRepairOrder/manageRepairOrder.js
+++ force-app/main/default/lwc/manageRepairOrder/manageRepairOrder.js
@@ -1,33 +1,79 @@
-import getFilteredAccounts from '@salesforce/apex/DataTableController.getFilteredAccounts';
-import getRepOrdDataTable from '@salesforce/apex/ManRepOrdTechController_Main.getRepOrdDataTable';
-import { LightningElement, wire, api } from 'lwc';
+import fetchRepairOrders from '@salesforce/apex/ManRepOrdTechController_Main.fetchRepairOrders';
+import { LightningElement, track } from 'lwc';
 import { NavigationMixin } from 'lightning/navigation';
+import { api } from 'lwc';
 import { refreshApex } from "@salesforce/apex";
+import { wire } from 'lwc';
 
-const actions = [
+const ACTIONS = [
     { label: 'View', name: 'view' },
     { label: 'Edit', name: 'edit' },
-    // { label: 'Delete', name: 'delete' },
 ];
 
 const COLUMNS = [
-    { label: 'Name', fieldName: 'Name', type: 'text' },
-    { label: 'Rep. Order', fieldName: 'Name', cellAttributes: { alignment: 'left' } },
-    { label: 'Technician', fieldName: 'TechnicianFK__c', cellAttributes: { alignment: 'left' } },
-    { label: 'Parts', fieldName: 'ReplacementPart_FK__c', cellAttributes: { alignment: 'left' } },
-    { label: 'Actions', type: 'action', typeAttributes: { rowActions: actions }, cellAttributes: { alignment: 'center' } }
+    { label: 'Name', fieldName: 'Name', type: 'text', cellAttributes: { alignment: 'left' } },
+    {
+        label: 'Actions',
+        type: 'action',
+        typeAttributes: { rowActions: ACTIONS },
+    },
 ];
 
-export default class DataTableExample extends LightningElement {
+export default class DataTableExample extends NavigationMixin(LightningElement) {
+    @track orders;
+    @track error;
+    @track columns = COLUMNS;
 
-    searchTerm = '';
-    orders = [];
-    columns = COLUMNS;
+    handleKeyChange(event) {
+        const searchTerm = event.target.value;
 
-    @wire(getRepOrdDataTable, { searchTerm: '$searchTerm' })
-    wiredAccounts({ error, data }) {
-        if (data) { this.orders = data; }
-        else if (error) { console.error(error); }
+        if (searchTerm) {
+            console.log(searchTerm);
+            fetchRepairOrders({ searchTerm })
+                .then(result => {
+                    this.orders = result;
+                }
+                )
+                .catch(error => {
+                    this.error = error;
+                }
+                );
+        }
+        else {
+            this.orders = undefined;
+        }
     }
-    handleSearchChange(event) { this.searchTerm = event.target.value; }
+
+    handleRowAction(event) {
+        const actionName = event.detail.action.name;
+        const row = event.detail.row;
+
+        switch (actionName) {
+            case 'view':
+                this[NavigationMixin.Navigate](
+                    {
+                        type: 'standard__recordPage',
+                        attributes:
+                        {
+                            recordId: row.Id,
+                            actionName: 'view'
+                        }
+                    }
+                );
+                break;
+            case 'edit':
+                this[NavigationMixin.Navigate](
+                    {
+                        type: 'standard__recordPage',
+                        attributes: {
+                            recordId: row.Id,
+                            objectApiName: 'Account',
+                            actionName: 'edit'
+                        }
+                    }
+                );
+                break;
+            default:
+        }
+    }
 }
